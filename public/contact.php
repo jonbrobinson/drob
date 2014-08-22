2b94e8980b84f97e0963559a9143187a7461064a
jonathan robinson <jonbrobinson@gmail.com>
2014-08-22 15:23:26 -0500

    Moved functions and variables to top, Created if/else statement to send mail or deliver error message

--- public/contact.php
+++ public/contact.php
@@ -1,30 +1,45 @@
 <?php
 
-if (isset($_POST['email'])) {
-	// Edit the 2 lines below as required
-	$email_to = "jonbrobinson@gmail.com";
-	$email_subject = "Danielle Contact Form";
+require_once 'init.php';
 
-	function died ($error) {
-		// your error code can go here
-		echo "We are very sorry, but there were error(s) found with the form you submitted. ";
-		echo "These errors appear below.<br><br>";
-		echo $error."<br><br>";
-		echo "Please go back and fix these errors.<br><br>";
-		exit();
-	}
+function died ($error) {
+	// your error code can go here
+	echo "We are very sorry, but there were error(s) found with the form you submitted. ";
+	echo "These errors appear below.<br><br>";
+	echo $error."<br><br>";
+	echo "Please go back and fix these errors.<br><br>";
+	exit();
+}
 
-	if(!isset($_POST['name']) ||
-		!isset($_POST['email']) ||
-		!isset($_POST['subject']) ||
-		!isset($_POST['content'])){
-		died ('We are sorry, but there appears to be a problem with the form you submitted.');
-	}
+function clean_string ($string) {
+	$bad = array("content-type", "bcc", "to", "cc", "href");
+	return str_replace ($bad,"",$string);
+}
 
-	$full_name = $_POST['name'];
-	$email = $_POST['email'];
-	$subject = $_POST['subject'];
-	$content = $_POST['content'];
+$full_name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$content = $_POST['content'];
+$email_to = "jonbrobinson@gmail.com";
+$email_subject = "Danielle Contact Form";
+
+if(!isset($_POST['name']) || !isset($_POST['email']) || !isset($_POST['subject']) || !isset($_POST['content'])) {
+
+	$email_message = "Form details below. \n\n";
+	$email_message .= "First Name: ".clean_string($full_name)."\n";
+	$email_message .= "Email: ".clean_string($email)."\n";
+	$email_message .= "Subject: ".clean_string($subject)."\n";
+	$email_message .= "Message: ".clean_string($content)."\n";
+
+	// Create Email Headers
+
+	$headers = 'From: '.$email."\r\n".
+	'Reply-To: '.$email."\r\n" .
+	'X-Mailer: PHP/' . phpversion();
+
+	mail($email_to, $email_subject, $email_message, $headers);
+
+} else {
 
 	$error_message = "";
 	$email_exp = "/^[A-za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/";
@@ -40,7 +55,7 @@ if (isset($_POST['email'])) {
 	}
 
 	if (strlen($subject) < 1) {
-		$error_message .= 'The Message you entered does not appear to be valid. <br>';
+		$error_message .= 'The Subject you entered does not appear to be valid. <br>';
 	}
 
 	if (strlen($content) < 2) {
@@ -51,24 +66,5 @@ if (isset($_POST['email'])) {
 		died($error_message);
 	}
 
-	$email_message = "Form details below. \n\n";
-
-	function clean_string ($string) {
-		$bad = array("content-type", "bcc", "to", "cc", "href");
-		return str_replace ($bad,"",$string);
-	}
-
-	$email_message .= "First Name: ".clean_string($full_name)."\n";
-	$email_message .= "Email: ".clean_string($email)."\n";
-	$email_message .= "Subject: ".clean_string($subject)."\n";
-	$email_message .= "Message: ".clean_string($content)."\n";
-
-	// Create Email Headers
-
-	$headers = 'From: '.$email."\r\n".
-	'Reply-To: '.$email."\r\n" .
-	'X-Mailer: PHP/' . phpversion();
-
-	mail($email_to, $email_subject, $email_message, $headers);
 }
 ?>
